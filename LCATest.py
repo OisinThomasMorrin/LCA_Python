11243dd50211605d3ad41db69b73ed63a76fd522
Oisin Morrin <morrino@tcd.ie>
2020-10-20 18:19:57 +0100

    Add additional unit tests

--- LCATest.py
+++ LCATest.py
@@ -1,7 +1,7 @@
 import unittest
 from LCA import Node
 from LCA import findLCA
-
+from LCA import findPath
 
 class TestLCA(unittest.TestCase):
 
@@ -25,6 +25,33 @@ class TestLCA(unittest.TestCase):
         root.right.right = Node(7)
         self.assertEqual(findLCA(root, 4, 6), 1)
 
+    def test_empty_binary_tree(self):
+        root = None
+        self.assertEqual(findLCA(root, 1,2), -1)
+    
+    def test_root_only_binary_tree(self):
+        root = Node(1)
+        self.assertEqual(findLCA(root,2,3),-1)
+    
+    def test_find_path_empty(self):
+        root = None
+        x = Node(0)
+        self.assertEqual(findPath(root,[],x), False)
+    
+    def test_find_path_only_root(self):
+        root = Node(1)
+        x = Node(0)
+        self.assertEqual(findPath(root,[],x), False)
+    
+    def test_check_for_k(self):
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        root.right.left = Node(6)
+        root.right.right = Node(7)
+        self.assertEqual(findPath(root, [], 2), True)
 
 if __name__ == '__main__':
     unittest.main()
