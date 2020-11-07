2bd4f9041a65bd5f8cc5c9ab939200ef8256f3e4
DESKTOP-5GLP31S\User <morrino@tcd.ie>
2020-11-07 19:29:09 +0000

    Add implementation of LCA for base cases

--- LCA.py
+++ LCA.py
@@ -5,35 +5,11 @@ class Node:
         self.succ = []
   
 
-def findPath( root, path, k): 
-  
-    if root is None: 
-        return False
-  
-    path.append(root.key) 
-  
-    if root.key == k : 
-        return True
-  
-    if ((root.left != None and findPath(root.left, path, k)) or
-            (root.right!= None and findPath(root.right, path, k))): 
-        return True 
-  
-       
-    path.pop() 
-    return False
-  
-def findLCA(root, n1, n2): 
-  
-    path1 = [] 
-    path2 = [] 
-  
-    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)): 
-        return -1 
-  
-    i = 0 
-    while(i < len(path1) and i < len(path2)): 
-        if path1[i] != path2[i]: 
-            break
-        i += 1
-    return path1[i-1] 
+def dagLCA(root,node_1,node_2):
+
+    if root is None:
+        return None
+    elif root.key == node_1 or root.key == node_2:
+        return root
+    elif node_1 == node_2:
+        return node_1.key
